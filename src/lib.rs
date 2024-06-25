a2a2093f54a78b6187bb32b3f3772d90a8a0f373
Matilde Morrone <contact@morrone.dev>
2024-06-25 16:41:42 +0200

    Add Device struct to api

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use raw_window_handle::{DisplayHandle, DrmDisplayHandle};
+use rustix::fd::{AsRawFd, OwnedFd};
+
+#[derive(Debug)]
+pub struct Device {
+    fd: OwnedFd,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Device {
+    pub unsafe fn new(fd: OwnedFd) -> Self {
+        Self { fd }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn display_handle(&self) -> DisplayHandle {
+        unsafe { DisplayHandle::borrow_raw(DrmDisplayHandle::new(self.fd.as_raw_fd()).into()) }
     }
 }
