f2e1eb4a78f53901d85db60397e89dbda5803202
Federico Maria Morrone <contact@morrone.dev>
2024-06-25 23:45:41 +0200

    Implement DRM_IOCTL_MODE_GETRESOURCES and refactor ioctl code

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use std::{
     ffi::{c_int, CString},
-    ptr::null_mut,
+    mem,
 };
 
-use linux_raw_sys::drm::{drm_version, DRM_IOCTL_BASE};
+use linux_raw_sys::drm::{drm_mode_card_res, drm_version, DRM_IOCTL_BASE};
 use raw_window_handle::{DisplayHandle, DrmDisplayHandle};
 use rustix::{
-    fd::{AsFd, AsRawFd, OwnedFd},
+    fd::{AsRawFd, OwnedFd},
     io,
     ioctl::{ioctl, ReadWriteOpcode, Updater},
 };
@@ -25,27 +25,17 @@ impl Device {
         unsafe { DisplayHandle::borrow_raw(DrmDisplayHandle::new(self.fd.as_raw_fd()).into()) }
     }
 
+    #[inline]
+    fn ioctl_rw<const NUM: u8, T>(&self, data: &mut T) -> io::Result<()> {
+        unsafe {
+            ioctl(
+                &self.fd,
+                Updater::<ReadWriteOpcode<DRM_IOCTL_BASE, NUM, T>, T>::new(data),
+            )
+        }
+    }
+
     pub fn version(&self) -> io::Result<Version> {
-        #[inline]
-        fn call_version_ioctl<F: AsFd>(fd: F, version: &mut drm_version) -> io::Result<()> {
-            unsafe {
-                ioctl(
-                    fd,
-                    Updater::<ReadWriteOpcode<DRM_IOCTL_BASE, 0x00, drm_version>, drm_version>::new(
-                        version,
-                    ),
-                )
-            }
-        }
-
-        #[inline]
-        fn create_and_reserve_buf(len: usize) -> Vec<u8> {
-            let mut buf = Vec::with_capacity(len);
-            buf.reserve_exact(len);
-
-            buf
-        }
-
         #[inline]
         unsafe fn ret_vector_to_cstring(mut vec: Vec<u8>, len: usize) -> CString {
             vec.set_len(len);
@@ -59,29 +49,19 @@ impl Device {
             CString::from_vec_with_nul_unchecked(vec)
         }
 
-        let mut version = Box::new(drm_version {
-            version_major: 0,
-            version_minor: 0,
-            version_patchlevel: 0,
-            name_len: 0,
-            name: null_mut(),
-            date_len: 0,
-            date: null_mut(),
-            desc_len: 0,
-            desc: null_mut(),
-        });
+        let mut version: Box<drm_version> = Box::new(unsafe { mem::zeroed() });
 
-        call_version_ioctl(&self.fd, &mut version)?;
+        self.ioctl_rw::<0x00, drm_version>(&mut version)?;
 
-        let mut name_buf = create_and_reserve_buf(version.name_len as usize + 1);
-        let mut date_buf = create_and_reserve_buf(version.date_len as usize + 1);
-        let mut desc_buf = create_and_reserve_buf(version.desc_len as usize + 1);
+        let mut name_buf: Vec<u8> = create_and_reserve_buf(version.name_len as usize + 1);
+        let mut date_buf: Vec<u8> = create_and_reserve_buf(version.date_len as usize + 1);
+        let mut desc_buf: Vec<u8> = create_and_reserve_buf(version.desc_len as usize + 1);
 
         version.name = name_buf.as_mut_ptr().cast();
         version.date = date_buf.as_mut_ptr().cast();
         version.desc = desc_buf.as_mut_ptr().cast();
 
-        call_version_ioctl(&self.fd, &mut version)?;
+        self.ioctl_rw::<0x00, drm_version>(&mut version)?;
 
         unsafe {
             let name = ret_vector_to_cstring(name_buf, version.name_len as usize);
@@ -98,6 +78,54 @@ impl Device {
             })
         }
     }
+
+    pub fn get_resources(&self) -> io::Result<Resources> {
+        let mut resources: drm_mode_card_res = unsafe { mem::zeroed() };
+
+        self.ioctl_rw::<0xA0, drm_mode_card_res>(&mut resources)?;
+
+        let mut fbs: Vec<u32> = create_and_reserve_buf(resources.count_fbs as usize);
+        let mut crtcs: Vec<u32> = create_and_reserve_buf(resources.count_crtcs as usize);
+        let mut connectors: Vec<u32> = create_and_reserve_buf(resources.count_connectors as usize);
+        let mut encoders: Vec<u32> = create_and_reserve_buf(resources.count_encoders as usize);
+
+        resources.fb_id_ptr = fbs.as_mut_ptr() as _;
+        resources.crtc_id_ptr = crtcs.as_mut_ptr() as _;
+        resources.connector_id_ptr = connectors.as_mut_ptr() as _;
+        resources.encoder_id_ptr = encoders.as_mut_ptr() as _;
+
+        self.ioctl_rw::<0xA0, drm_mode_card_res>(&mut resources)?;
+
+        unsafe {
+            fbs.set_len(resources.count_fbs as usize);
+            crtcs.set_len(resources.count_crtcs as usize);
+            connectors.set_len(resources.count_connectors as usize);
+            encoders.set_len(resources.count_encoders as usize);
+        }
+
+        Ok(Resources {
+            fbs,
+            crtcs,
+            connectors,
+            encoders,
+            min_width: resources.min_width,
+            max_width: resources.max_width,
+            min_height: resources.min_height,
+            max_height: resources.max_height,
+        })
+    }
+}
+
+#[inline]
+fn create_and_reserve_buf<T>(len: usize) -> Vec<T> {
+    if len == 0 {
+        return Vec::new();
+    }
+
+    let mut buf = Vec::with_capacity(len);
+    buf.reserve_exact(len);
+
+    buf
 }
 
 #[derive(Debug)]
@@ -108,4 +136,16 @@ pub struct Version {
     pub name: CString,
     pub date: CString,
     pub desc: CString,
+}
+
+#[derive(Debug)]
+pub struct Resources {
+    pub fbs: Vec<u32>,
+    pub crtcs: Vec<u32>,
+    pub connectors: Vec<u32>,
+    pub encoders: Vec<u32>,
+    pub min_width: u32,
+    pub max_width: u32,
+    pub min_height: u32,
+    pub max_height: u32,
 }
