5ad5881f98239ccd4b32f9879c30b88a07bba8db
Federico Maria Morrone <contact@morrone.dev>
2024-06-26 01:23:13 +0200

    Implement DRM_IOCTL_MODE_GETCONNECTOR

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 use std::{
     ffi::{c_int, CString},
+    fmt::Display,
     mem,
+    num::NonZeroU32,
 };
 
-use linux_raw_sys::drm::{drm_mode_card_res, drm_version, DRM_IOCTL_BASE};
+use linux_raw_sys::drm::{
+    drm_mode_card_res, drm_mode_get_connector, drm_mode_modeinfo, drm_version, DRM_IOCTL_BASE,
+};
 use raw_window_handle::{DisplayHandle, DrmDisplayHandle};
 use rustix::{
     fd::{AsRawFd, OwnedFd},
@@ -16,6 +20,7 @@ pub struct Device {
     fd: OwnedFd,
 }
 
+// FIXME: take into account hotplug
 impl Device {
     pub unsafe fn new(fd: OwnedFd) -> Self {
         Self { fd }
@@ -86,7 +91,8 @@ impl Device {
 
         let mut fbs: Vec<u32> = create_and_reserve_buf(resources.count_fbs as usize);
         let mut crtcs: Vec<u32> = create_and_reserve_buf(resources.count_crtcs as usize);
-        let mut connectors: Vec<u32> = create_and_reserve_buf(resources.count_connectors as usize);
+        let mut connectors: Vec<ConnectorId> =
+            create_and_reserve_buf(resources.count_connectors as usize);
         let mut encoders: Vec<u32> = create_and_reserve_buf(resources.count_encoders as usize);
 
         resources.fb_id_ptr = fbs.as_mut_ptr() as _;
@@ -112,6 +118,68 @@ impl Device {
             max_width: resources.max_width,
             min_height: resources.min_height,
             max_height: resources.max_height,
+        })
+    }
+
+    pub fn get_connector(&self, connector_id: ConnectorId, probe: bool) -> io::Result<Connector> {
+        let stack_mode: drm_mode_modeinfo = unsafe { mem::zeroed() };
+        let mut connector: drm_mode_get_connector = unsafe { mem::zeroed() };
+
+        connector.connector_id = connector_id.into();
+
+        if !probe {
+            connector.count_modes = 1;
+            connector.modes_ptr = &stack_mode as *const _ as _
+        }
+
+        self.ioctl_rw::<0xA7, drm_mode_get_connector>(&mut connector)?;
+
+        let mut encoders: Vec<u32> = create_and_reserve_buf(connector.count_encoders as usize);
+        let mut modes: Vec<drm_mode_modeinfo> =
+            create_and_reserve_buf(connector.count_encoders as usize); // FIXME: handle special case where modes is empty
+        let mut props: Vec<u32> = create_and_reserve_buf(connector.count_props as usize);
+        let mut prop_values: Vec<u64> = create_and_reserve_buf(connector.count_props as usize);
+
+        connector.encoders_ptr = encoders.as_mut_ptr() as _;
+        connector.modes_ptr = modes.as_mut_ptr() as _;
+        connector.props_ptr = props.as_mut_ptr() as _;
+        connector.prop_values_ptr = prop_values.as_mut_ptr() as _;
+
+        self.ioctl_rw::<0xA7, drm_mode_get_connector>(&mut connector)?;
+
+        unsafe {
+            encoders.set_len(connector.count_encoders as usize);
+            modes.set_len(connector.count_modes as usize);
+            props.set_len(connector.count_props as usize);
+            prop_values.set_len(connector.count_props as usize);
+        }
+
+        let drm_mode_get_connector {
+            encoder_id,
+            connector_type,
+            connector_type_id,
+            connection,
+            mm_width,
+            mm_height,
+            subpixel,
+            pad,
+            ..
+        } = connector;
+
+        Ok(Connector {
+            encoder_id,
+            connector_id,
+            connector_type,
+            connector_type_id,
+            connection,
+            mm_width,
+            mm_height,
+            subpixel,
+            pad,
+            encoders,
+            modes,
+            props,
+            prop_values,
         })
     }
 }
@@ -142,10 +210,43 @@ pub struct Version {
 pub struct Resources {
     pub fbs: Vec<u32>,
     pub crtcs: Vec<u32>,
-    pub connectors: Vec<u32>,
+    pub connectors: Vec<ConnectorId>,
     pub encoders: Vec<u32>,
     pub min_width: u32,
     pub max_width: u32,
     pub min_height: u32,
     pub max_height: u32,
+}
+
+#[derive(Debug)]
+pub struct Connector {
+    pub encoder_id: u32,
+    pub connector_id: ConnectorId,
+    pub connector_type: u32,
+    pub connector_type_id: u32,
+    pub connection: u32,
+    pub mm_width: u32,
+    pub mm_height: u32,
+    pub subpixel: u32,
+    pub pad: u32,
+    pub encoders: Vec<u32>,
+    pub modes: Vec<drm_mode_modeinfo>,
+    pub props: Vec<u32>,
+    pub prop_values: Vec<u64>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(transparent)]
+pub struct ConnectorId(NonZeroU32);
+
+impl Display for ConnectorId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl From<ConnectorId> for u32 {
+    fn from(value: ConnectorId) -> Self {
+        value.0.get()
+    }
 }
